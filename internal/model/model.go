f728e5dc4248b097a584ab52acdd0f4f5ae1a931
Aibar <aibarkenbay@gmail.com>
2026-01-02 20:02:54 +0500

    Feat: add response models

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,1 +1,18 @@
 package model
+
+type StudentResponse struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Gender    string `json:"gender"`
+	BirthDate string `json:"birth_date"`
+	GroupName string `json:"group_name"`
+}
+
+type ScheduleResponse struct {
+	ID        int    `json:"id"`
+	Faculty   string `json:"faculty"`
+	Group     string `json:"group"`
+	Subject   string `json:"subject"`
+	ClassTime string `json:"class_time"`
+}
