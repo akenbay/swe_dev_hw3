00375df6e1ca0541cf2b8794268963d83464aac0
Aibar <aibarkenbay@gmail.com>
2026-01-31 17:02:07 +0500

    Feat: cors

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
@@ -21,6 +23,13 @@ func (s *Server) Start(addr string) error {
 	// Add global middleware
 	e.Use(middleware.Recover())
 	e.Use(middleware.RequestLogger())
+
+	// CORS: allow browser requests from any origin (e.g. frontend on different domain)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
 
 	// Register all routes (public and protected)
 	s.handler.Register(e)
